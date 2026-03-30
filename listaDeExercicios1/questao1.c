e511eb43d8de550b1056c7e7cb70e3f620339dfe
Luis Nunes <luisraj97@gmail.com>
2026-03-30 10:37:27 -0300

    Add questao1.c to print alternating patterns

---
+++ listaDeExercicios1/questao1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main() { 
+    int contador = 1;
+    while (contador <= 10) {
+    printf("%s\n", contador % 2 ? "****" : " + + + + + + + +");
+    ++contador;
+    }
+    return 0;
+}
